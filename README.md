a74be9f4c9d37680200f5d2f262593a17a648529
Rahul Dey <rahuldey832000@gmail.com>
2024-02-17 18:00:16 +0530

    Implement serilog

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SeriLog-Example-Asp.Net
+
+Implement logging in asp .net web api using SeriLog
---
+++ Serilog Example/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Serilog_Example.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
